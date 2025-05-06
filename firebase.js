4d662ffb1dc9c862d1607e859c1bbf27d265283e
brenoadebola <brenowebseo@gmail.com>
2025-05-05 23:47:06 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-storage.js";
+// Importe as funções que você precisa dos SDKs que você usa
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL, listAll } from "firebase/storage";
 
+// Sua configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAv3g3-Gjp2OYLNG3PgVczwBFSQXAxcDj0",
   authDomain: "maria-18e7a.firebaseapp.com",
   projectId: "maria-18e7a",
-  storageBucket: "maria-18e7a.appspot.com",
+  storageBucket: "maria-18e7a.firebasestorage.app",
   messagingSenderId: "187919316652",
   appId: "1:187919316652:web:153b6f5758859e14f25109"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
+// Inicialize o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicialize o Firebase Storage
+const storage = getStorage(app);
+
+export { storage, ref, uploadBytesResumable, getDownloadURL, listAll };
